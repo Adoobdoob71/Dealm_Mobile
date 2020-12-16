c825545b358d4b933b53c2b48aa40109331b19c7
Adoobdoob71 <eladelad71@gmail.com>
2020-12-16 15:15:34 +0200

    nothing tbh

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,65 +1,132 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View, ViewProps, TouchableNativeFeedback } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { ActivityIndicator, TouchableRipple, useTheme } from 'react-native-paper';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as React from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  ViewProps,
+  TouchableNativeFeedback,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import {
+  ActivityIndicator,
+  TouchableRipple,
+  useTheme,
+} from "react-native-paper";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 interface ButtonProps extends ViewProps {
   mode: "full" | "bordered" | "text";
-  text: string;
+  text?: string;
   icon?: string;
   disabled?: boolean;
   loading?: boolean;
   color?: string;
   onPress: () => void;
+  iconButton?: boolean;
 }
 
-function Button(props : ButtonProps){
+function Button(props: ButtonProps) {
   const { colors } = useTheme();
   const styles = StyleSheet.create({
     button: {
-      backgroundColor: props.disabled || props.loading ? (props.mode === "full" ? colors.disabled : "transparent") : props.mode === "full" ? colors.primary : "transparent",
+      backgroundColor:
+        props.disabled || props.loading
+          ? props.mode === "full"
+            ? colors.disabled
+            : "transparent"
+          : props.mode === "full"
+          ? colors.primary
+          : "transparent",
       borderRadius: 6,
       borderWidth: props.mode === "bordered" ? 1 : undefined,
-      borderColor: props.mode === "bordered" ? (props.disabled || props.loading ? colors.disabled : colors.primary) : undefined,
+      borderColor:
+        props.mode === "bordered"
+          ? props.disabled || props.loading
+            ? colors.disabled
+            : colors.primary
+          : undefined,
       flexDirection: "row",
       paddingHorizontal: 12,
       paddingVertical: 10,
-      alignItems: "center"
+      alignItems: "center",
     },
     left: {
-      marginEnd: 10
+      marginEnd: 10,
     },
     text: {
-      color: props.mode === "full" ? (props.disabled || props.loading ? colors.disabled : "#FFFFFF") : props.disabled || props.loading ? colors.disabled : colors.primary,
+      color:
+        props.mode === "full"
+          ? props.disabled || props.loading
+            ? colors.disabled
+            : "#FFFFFF"
+          : props.disabled || props.loading
+          ? colors.disabled
+          : colors.primary,
       fontSize: 14,
       fontWeight: "bold",
       textTransform: "uppercase",
-      opacity: props.disabled || props.loading ? 0.6 : 1
-    }
-  })
+      opacity: props.disabled || props.loading ? 0.6 : 1,
+      display: props.text ? "flex" : "none",
+    },
+  });
   return (
-    <View style={[{ borderRadius: 6, overflow: "hidden", elevation: props.mode === "full" ? 1 : 0 }, props.style]}>
-      <TouchableRipple 
-        onPress={props.onPress} 
+    <View
+      style={[
+        {
+          borderRadius: 6,
+          overflow: "hidden",
+          elevation: props.mode === "full" ? 1 : 0,
+        },
+        props.style,
+      ]}>
+      <TouchableRipple
+        onPress={props.onPress}
         disabled={props.loading || props.disabled}
-        rippleColor={Platform.OS === "web" ? "#ff8e7175" : undefined} 
-        background={Platform.OS === "android" ? TouchableNativeFeedback.Ripple(colors.primary, true) : undefined}
-      >
+        rippleColor={Platform.OS === "web" ? "#ff8e7175" : undefined}
+        background={
+          Platform.OS === "android"
+            ? TouchableNativeFeedback.Ripple(colors.primary, true)
+            : undefined
+        }>
         <View style={styles.button}>
           {props.loading ? (
-            <ActivityIndicator size={16} color={props.mode === "full" ? (props.disabled || props.loading ? colors.disabled : "#FFFFFF") : props.disabled || props.loading ? colors.disabled : colors.primary} style={styles.left}/>
-          ) : props.icon && 
-            <MaterialCommunityIcons name={props.icon} size={18} color={props.mode === "full" ? (props.disabled || props.loading ? colors.disabled : "#FFFFFF") : props.disabled || props.loading ? colors.disabled : colors.primary} style={styles.left} />
-          }
+            <ActivityIndicator
+              size={16}
+              color={
+                props.mode === "full"
+                  ? props.disabled || props.loading
+                    ? colors.disabled
+                    : "#FFFFFF"
+                  : props.disabled || props.loading
+                  ? colors.disabled
+                  : colors.primary
+              }
+              style={styles.left}
+            />
+          ) : (
+            props.icon && (
+              <MaterialCommunityIcons
+                name={props.icon}
+                size={18}
+                color={
+                  props.mode === "full"
+                    ? props.disabled || props.loading
+                      ? colors.disabled
+                      : "#FFFFFF"
+                    : props.disabled || props.loading
+                    ? colors.disabled
+                    : colors.primary
+                }
+                style={styles.left}
+              />
+            )
+          )}
           <Text style={styles.text}>{props.text}</Text>
         </View>
-     </TouchableRipple>
+      </TouchableRipple>
     </View>
-  )
+  );
 }
 
-export {
-  Button,
-  ButtonProps
-}
+export { Button, ButtonProps };
