1911bdf7e22c5a3f7046692f84268ba232eb181d
Adoobdoob71 <eladelad71@gmail.com>
2021-02-09 10:53:26 +0200

    🐛 FIX: small bug fix

---
+++ firebaseconfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyChey4oljhd6lxuyd_VfbvFcPW6MwfZPTE",
+  authDomain: "dealm-85e42.firebaseapp.com",
+  projectId: "dealm-85e42",
+  storageBucket: "dealm-85e42.appspot.com",
+  messagingSenderId: "891093598191",
+  appId: "1:891093598191:web:18777dd9d7de0bb5ce001a",
+  measurementId: "G-LVPRQYHN21",
+};
